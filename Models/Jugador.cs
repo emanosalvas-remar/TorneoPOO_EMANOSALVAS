c53c918cf28b4edc74ba5f4b0d27e7fa0bf238d6
emanosalvas-remar <68337935+emanosalvas-remar@users.noreply.github.com>
2026-07-02 21:21:58 -0500

    Agregar archivos de proyecto.

---
+++ Models/Jugador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorneoPOO_EMANOSALVAS.Models
+{
+    public class Jugador
+    {
+        //ATRIBUTOS O CARACTERISTICAS
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public int Numero { get; set; }
+        public string Posicion { get; set; }
+
+        //METODOS, COMPORTAMIENTOS O FUNCIONES
+        public void Presentar()
+        {
+            Console.WriteLine($"Hola soy {this.Nombre} tengo {this.Edad} años y mi número es el {this.Numero}");
+        }
+    }
+}
